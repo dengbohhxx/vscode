41cc774a723531d7c199de03a8c8dcccc876fed9
dengbohhxx <dengbohhxx@sina.com>
2020-09-28 13:09:06 +0800

    add string

---
+++ 动态规划/string.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s("annabella");
+    string s1("bella");
+    string::size_type pos1=s.find("bella");
+    if(pos1!=string::npos)
+    {
+        cout<<" find it"<<endl;
+    }
+    else
+    {
+        cout<<"not find!!!"<<endl;
+    }
+    string name("r2d3");
+    string  num("0123456789");
+    string::size_type pos=0;
+    while((pos=name.find_first_of(num,pos))!=string::npos)
+    {
+        cout<<name[pos]<<endl;
+        pos++;
+    }
+    return 0;
+}

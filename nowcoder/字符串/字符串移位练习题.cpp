246623646f4b5ed95a3f398fcf3533b96d648f19
dengbohhxx <dengbohhxx@sina.com>
2020-09-28 21:09:31 +0800

    字符串移位

--- nowcoder/字符串/字符串移位练习题.cpp
+++ nowcoder/字符串/字符串移位练习题.cpp
@@ -8,3 +8,24 @@ public:
         return A;
     }
 };
+class Translation {
+public:
+    void reverse(string &s,int first,int last)
+    {
+        int f=first;
+        int l=last;
+        while(f<l)
+        {
+            swap(s[f],s[l]);
+            f++;
+            l--;
+        }
+    }
+    string stringTranslation(string A, int n, int len) {
+        // write code here
+        reverse(A,0,len-1);
+        reverse(A,len,n-1);
+        reverse(A,0,n-1);
+        return A;
+    }
+};

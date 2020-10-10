af3814e448f7f5e60493aec6db0e6ac33fb6a84f
dengbohhxx <dengbohhxx@sina.com>
2020-10-10 19:23:58 +0800

    复杂链表的复制

---
+++ nowcoder/链表/复杂链表的复制练习题.cpp
@@ -0,0 +1,59 @@
+/*
+struct RandomListNode {
+    int label;
+    struct RandomListNode *next, *random;
+    RandomListNode(int x) :
+            label(x), next(NULL), random(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    RandomListNode* Clone(RandomListNode* pHead)
+    {
+        if(pHead == NULL)
+        {
+            return NULL;
+        }
+        
+        RandomListNode* pNode = pHead;
+        // 对于每个结点 复制一个一模一样的结点
+        while (pNode)
+        {
+            RandomListNode* temp = new RandomListNode(pNode->label);
+            //temp->label = pNode->label;
+            temp->next = pNode->next;
+            //temp->random = NULL;
+            
+            pNode->next = temp;	
+            pNode = temp->next;			// 源链表的下一个结点
+        }
+        
+        pNode = pHead;
+        // 指定新分配的每个结点的random(实际上就是源结点的random的next)
+        while (pNode)
+        {
+            RandomListNode* temp = pNode->next;
+            // 如果源结点的random不指向NULL 为新链表的结点分配random属性?????????
+            if(pNode->random)
+            {
+                temp->random = pNode->random->next;
+            }
+            pNode = temp->next;
+        }
+        
+        // 分解两个链表
+        pNode = pHead;
+        RandomListNode* temp;
+        RandomListNode *newHead = pHead->next;
+        while(pNode->next)
+        {
+            temp = pNode->next;
+            pNode->next = temp->next;
+            pNode = temp;
+        }
+        
+        return newHead;
+
+    }
+};

305fc7d68e3ed5fc547756c517f4968b4babd5d2
dengbohhxx <dengbohhxx@sina.com>
2020-10-24 11:22:30 +0800

    完全二叉树判断练习题

--- nowcoder/二叉树/完全二叉树判断练习题.cpp
+++ nowcoder/二叉树/完全二叉树判断练习题.cpp
@@ -8,43 +8,41 @@ struct TreeNode {
     }
 };*/
 
-class CheckCompletion {
+class CheckCompletion
+{
 public:
-    bool chk(TreeNode* root) {
+    bool chk(TreeNode *root)
+    {
         // write code here
-        queue<TreeNode*> que;//队列，二叉树按行遍历
-        TreeNode* current =NULL;
+        queue<TreeNode *> que; //队列，二叉树按行遍历
+        TreeNode *current = NULL;
         que.push(root);
-        bool flag=false;//用来标记是否进入左孩子不为空右孩子不为空，则后续均为叶子结点的情况
-        while(!que.empty())
+        bool flag = false; //用来标记是否进入左孩子不为空右孩子不为空，则后续均为叶子结点的情况
+        while (!que.empty())
         {
-            current=que.front();
+            current = que.front();
             que.pop();
-            if(current->left!=NULL&&current->right!=NULL)//左右孩子均不为空，压入队列
+            if (current->left != NULL && current->right != NULL) //左右孩子均不为空，压入队列
             {
                 que.push(current->left);
                 que.push(current->right);
             }
-            if(current->left==NULL&&current->right!=NULL)//左孩子为空右孩子不为空，false
+            if (current->left == NULL && current->right != NULL) //左孩子为空右孩子不为空，false
             {
                 return false;
             }
-            if(current->left!=NULL&&current->right==NULL)//左孩子不为空右孩子不为空，则后续均为叶子结点
+            if (current->left != NULL && current->right == NULL) //左孩子不为空右孩子不为空，则后续均为叶子结点
             {
-                if(flag)
-                {
-                    if(current->left!=NULL||current->right!=NULL)
-                        return false;
-                }
-                flag=true;
+
+                flag = true;
                 continue;
             }
-            if(flag)
-              {
-               if(current->left!=NULL||current->right!=NULL)
-                return false;
-              }
+            if (flag)
+            {
+                if (current->left != NULL || current->right != NULL)
+                    return false;
+            }
         }
-      return true;
+        return true;
     }
 };

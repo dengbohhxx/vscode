92d20c5e690905d765c6493efaa0accad296b630
dengbohhxx <dengbohhxx@sina.com>
2020-10-30 19:23:50 +0800

    红娘最大匹配问题

--- 匈牙利算法/红娘_最大匹配问题.cpp
+++ 匈牙利算法/红娘_最大匹配问题.cpp
@@ -1,32 +1,85 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-i
-int M, N;            //M, N分别表示左、右侧集合的元素数量
-int Map[MAXM][MAXN]; //邻接矩阵存图
-int p[MAXN];         //记录当前右侧元素所对应的左侧元素
-bool vis[MAXN];      //记录右侧元素是否已被访问过
-bool match(int i)
+//判断一个数是否为素数
+bool IsPrimer(int n)
 {
-    for (int j = 1; j <= N; ++j)
-        if (Map[i][j] && !vis[j]) //有边且未访问
+    for (int i = 2; i * i < n; i++)
+    {
+        if (n % i == 0)
         {
-            vis[j] = true;                 //记录状态为访问过
-            if (p[j] == 0 || match(p[j])) //如果暂无匹配，或者原来匹配的左侧元素可以找到新的匹配
+            return false;
+        }
+    }
+    return true;
+}
+//匈牙利算法，为某一个目标奇数找到它的素数伴侣（偶数）,皆以下标表示
+bool FindMate(const int n, vector<vector<bool>>& map, vector<int>& match, vector<bool> &vis)
+{
+    for (int i = 0; i < match.size(); i++)
+    {
+        if (!vis[i] && map[i][n])//偶数未被访问过并且能与目标奇数组成素数（有关系）
+        {
+            vis[i] = true;
+            if (match[i] == -1 || FindMate(match[i], map, match, vis))//当前偶数没有匹配或能给被抛弃的奇数找到新的偶数
             {
-                p[j] = i;    //当前左侧元素成为当前右侧元素的新匹配
-                return true; //返回匹配成功
+                match[i] = n;//找到这个偶数
+                return true;
             }
         }
-    return false; //循环结束，仍未找到匹配，返回匹配失败
-}
-int Hungarian()
-{
-    int cnt = 0;
-    for (int i = 1; i <= M; ++i)
-    {
-        memset(vis, 0, sizeof(vis)); //重置vis数组
-        if (match(i))
-            cnt++;
     }
-    return cnt;
+    return false;
+}
+
+int main()
+{
+    int num = 0;
+    while (cin >> num)
+    {
+        int count = 0;//匹配对数
+        vector<int> even;//偶数
+        vector<int> odd;//奇数
+        int val = 0;
+        //读取数据
+        while (num--)
+        {
+            cin >> val;
+            if (val % 2 == 0)
+            {
+                odd.push_back(val);
+            }
+            else
+            {
+                even.push_back(val);
+            }
+        }
+        if (odd.empty() || even.empty())
+        {
+            cout << count << endl;
+            continue;
+        }
+        //建立关系表，图中的边
+        vector<vector<bool>> map(even.size(), vector<bool>(odd.size(), false));
+        for (int i = 0; i < even.size(); i++)
+        {
+            for (int j = 0; j < odd.size(); j++)
+            {
+                if (IsPrimer(even[i] + odd[j]))
+                {
+                    map[i][j] = true;
+                }
+            }
+        }
+        //建立初始匹配表
+        vector<int> match(even.size(), -1);    
+        //为每一个奇数都尝试去找对应的偶数，    
+        for (int i = 0; i < odd.size(); i++)
+        {
+            vector<bool> vis(even.size(), false);//每一次查找都相当于重新分配，标志要清零
+            if (FindMate(i, map, match, vis))
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+    }
 }

14f18fb7930d9629603e998420c830f944ff02e2
dengbohhxx <dengbohhxx@sina.com>
2020-10-31 02:01:22 +0800

    famgpingguo

--- nowcoder/huawei/放苹果.cpp
+++ nowcoder/huawei/放苹果.cpp
@@ -2,13 +2,12 @@
 using namespace std;
 int digui(int m,int n)
 {
- if (n>m)
-        return digui(m,m);
-    if (m==0)
+    if(m==0||n==1)
         return 1;
-    if (n==0)
-        return 0;
-    return digui(m,n-1)+digui(m-n,n);
+    else if(m<n)
+        return digui(m,m);
+     else
+         return digui(m-n,n)+digui(m,n-1);
 }
 int main()
 {

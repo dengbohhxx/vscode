b16eda0b8eaba9bc74dfc89e59d4c4aaa79a6c70
dengbohhxx <dengbohhxx@sina.com>
2020-11-21 10:46:03 +0800

    从尾到头打印链表

--- nowcoder/牛客题霸/从尾到头打印链表.cpp
+++ nowcoder/牛客题霸/从尾到头打印链表.cpp
@@ -7,6 +7,7 @@
 *        }
 *  };
 */
+/*
 class Solution {
 public:
     void reverselist(ListNode* head,vector<int> &vec)
@@ -24,6 +25,28 @@ public:
         if(head==NULL)
             return vec;
         reverselist(head,vec);
+        return vec;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        vector<int> vec;
+        stack<ListNode*> sta;
+        if(head==nullptr)
+            return vec;
+        ListNode* phead=head;
+        while(phead!=nullptr)
+        {
+            sta.push(phead);
+            phead=phead->next;
+        }
+        while(!sta.empty())
+        {
+            vec.push_back(sta.top()->val);
+            sta.pop();
+        }
         return vec;
     }
 };

0330ed5f7029efbeab84fc96d89718e29ad0056b
dengbohhxx <dengbohhxx@sina.com>
2020-11-25 21:34:19 +0800

    平衡二叉树

--- 剑指offer/平衡二叉树.cpp
+++ 剑指offer/平衡二叉树.cpp
@@ -1,12 +1,12 @@
-class Solution {
+;class Solution {
 public:
-    int getheight(TreeNode* node,int h,bool &res)
+    int getheight(TreeNode* node,bool &res)
     {
         if(node==NULL)
             return 0;
 
-         int lefth=getheight(node->left,h+1,res);
-         int righth=getheight(node->right,h+1,res);
+         int lefth=getheight(node->left,res);
+         int righth=getheight(node->right,res);
          if(abs(lefth-righth)>1)
              res=false;
          return max(lefth,righth)+1;
@@ -15,7 +15,7 @@ public:
     bool IsBalanced_Solution(TreeNode* pRoot) {
     
         bool res=true;
-        int h=getheight(pRoot,0,res);
+        int h=getheight(pRoot,res);
         return res;
     }
 };

e3ba10b79dd343c9026b94f57f97716530856348
dengbohhxx <dengbohhxx@sina.com>
2020-11-29 22:18:21 +0800

    二叉搜索树第k个节点

--- 剑指offer/二叉搜索树的第k个节点.cpp
+++ 剑指offer/二叉搜索树的第k个节点.cpp
@@ -32,7 +32,43 @@ public:
             return NULL;
         else
         return vec[k-1];
+   
     }
+};
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    void kthNode(TreeNode* pRoot,TreeNode*& knode,int &index,int &k)
+    {
 
-    
+        if(pRoot==NULL)
+            return;
+        kthNode(pRoot->left,knode,index,k);
+        index++;
+        if(index==k)
+        {
+            knode=pRoot;
+            return;
+        }
+        if(index<k)
+            kthNode(pRoot->right,knode,index,k);
+    }
+    TreeNode* KthNode(TreeNode* pRoot, int k)
+    {
+        if(pRoot==NULL||k==0)
+            return NULL;
+        TreeNode* knode=NULL;
+        int index=0;
+        kthNode(pRoot,knode,index,k);
+        return knode;
+    }
 };

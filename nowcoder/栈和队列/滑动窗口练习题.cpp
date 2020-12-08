bc13ab9777f7b691dbb875e1bdcc93acf3a4dc9d
dengbohhxx <dengbohhxx@sina.com>
2020-12-09 03:54:01 +0800

    添加新方法

--- nowcoder/栈和队列/滑动窗口练习题.cpp
+++ nowcoder/栈和队列/滑动窗口练习题.cpp
@@ -1,3 +1,4 @@
+/*
 class SlideWindow {
 public:
     vector<int> slide(vector<int> arr, int n, int w) {
@@ -23,5 +24,32 @@ public:
         }
         res.push_back(arr[deq.front()]); 
         return res;
+    }
+};
+*/
+class SlideWindow {
+public:
+    vector<int> slide(vector<int> arr, int n, int w) {
+        // write code here
+        vector<int> vec;
+        if(w<1)//窗口小于1
+            return vec;
+        if(n<w)//窗口大于数组长度
+        {
+            vec.push_back(*max_element(arr.begin(),arr.end()));
+            return vec;
+        }
+        deque<int> deq;
+        for(int i=0;i<n;i++)
+        {
+            while(!deq.empty()&&arr[i]>=arr[deq.back()])
+                deq.pop_back();
+            deq.push_back(i);
+            if(!deq.empty()&&deq.front()==i-w)//left过期
+                deq.pop_front();
+            if(i>=w-1)//vec纪录最大值
+                vec.push_back(arr[deq.front()]);
+        }
+        return vec;
     }
 };

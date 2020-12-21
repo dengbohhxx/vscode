bdf34c741111bcacf018a7febbdffac81b638362
dengbohhxx <dengbohhxx@sina.com>
2020-12-21 23:17:37 +0800

    n queen

--- nowcoder/牛客题霸/N皇后问题.cpp
+++ nowcoder/牛客题霸/N皇后问题.cpp
@@ -11,3 +11,55 @@ public:
         return A[n-1];
     }
 };
+class Solution {
+public:
+    /**
+     * 
+     * @param n int整型 the n
+     * @return int整型
+     */
+    bool check(int p,vector<int> &vec)
+    {
+        for(int i=0;i<p;i++)
+        {
+            if(vec[i] == vec[p] || abs(vec[p] - vec[i]) == abs(p - i))
+                return 0;
+        }
+        return 1;
+        
+        
+    }
+    
+    void fun(int p,int n,vector<int> &vec,vector<vector<string> > &vvs)
+    {
+        if(p==n)
+        {
+            vector<string> str(n,string(n,'.'));  
+            for(int i=0;i<n;i++)
+            {
+                str[i][vec[i]]='Q';
+            }
+            vvs.push_back(str);
+        return;
+        }
+        for(vec[p]=0;vec[p]<n;vec[p]++)
+        {
+            if(check(p,vec)==true)
+                fun(p+1,n,vec,vvs);
+        }
+        
+        
+        
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<int> vec(n);
+        vector<vector<string> > vvs;
+        fun(0,n,vec,vvs);
+        return vvs;
+    }
+    int Nqueen(int n) {
+        // write code here
+        vector<vector<string>> str=solveNQueens(n);
+        return str.size();
+    }
+};

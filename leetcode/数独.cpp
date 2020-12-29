f41246cc3c59913567384df9f611204a47a87728
dengbohhxx <dengbohhxx@sina.com>
2020-12-29 21:05:00 +0800

    数独

---
+++ leetcode/数独.cpp
@@ -0,0 +1,40 @@
+void solveSudoku(vector<vector<char> > &board) {
+        dfs(board, 0, 0);
+    }
+
+    bool dfs(vector<vector<char> > &board, int x, int y) {
+        if (x == 9) 
+        { 
+            return true; 
+        }
+        if (y == 9)
+        { 
+            return dfs(board, x+1, 0);
+        }  // 跳转到下一行
+        if (board[x][y] != '.') 
+        { 
+            return dfs(board, x, y+1); 
+        }
+        for (char c = '1'; c <= '9'; ++c) {
+            if (!isValid(board, x, y, c)) 
+                continue;
+            board[x][y] = c;
+            if (dfs(board, x, y+1)) 
+                return true;
+            board[x][y] = '.';
+        }
+        return false;
+    }
+
+    bool isValid(vector<vector<char> > &board, int x, int y, char ch) {
+        for (int i = 0; i < 9; ++i) 
+        {
+            if (board[x][i] == ch) 
+                return false;
+            if (board[i][y] == ch) 
+                return false;
+            if (board[(x/3)*3 + i/3][(y/3)*3 + i%3] == ch) 
+                return false;
+        }
+        return true;
+    }

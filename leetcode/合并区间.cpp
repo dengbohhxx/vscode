bcd968bbe96635c30a482da4f139d5854bf5c86a
dengbohhxx <dengbohhxx@sina.com>
2021-01-01 05:12:44 +0800

    合并区间

---
+++ leetcode/合并区间.cpp
@@ -0,0 +1,24 @@
+bool cmp(vector<int> a,vector<int> b)
+{
+    return a[0]<b[0];
+}
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.size() == 0) {
+            return {};
+        }
+        sort(intervals.begin(), intervals.end(),cmp);
+        vector<vector<int>> merged;
+        for (int i = 0; i < intervals.size(); ++i) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (!merged.size() || merged.back()[1] < L) {
+                merged.push_back({L, R});
+            }
+            else {
+                merged.back()[1] = max(merged.back()[1], R);
+            }
+        }
+        return merged;
+    }
+};

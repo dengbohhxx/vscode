19144b84c3add043544d370b507bfc2d76408c39
dengbohhxx <dengbohhxx@sina.com>
2021-01-12 20:06:59 +0800

    二叉树最大深度

---
+++ leetcode/二叉树最大深度.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        else
+        {
+            int hl=maxDepth(root->left);
+            int hr=maxDepth(root->right);
+            int height=max(hl,hr)+1;
+            return height;
+        }
+    }
+};

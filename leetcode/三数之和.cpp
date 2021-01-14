3fad22dd0670caf45e3f5896bb108d44af6b016c
dengbohhxx <dengbohhxx@sina.com>
2021-01-14 14:23:12 +0800

    fixed

--- leetcode/三数之和.cpp
+++ leetcode/三数之和.cpp
@@ -1,56 +1,56 @@
 class Solution {
 public:
-
-   vector<vector<int>> twosum(vector<int> nums,int start,int end,int target,int value)
-   {
-       vector<vector<int>> answer;
-       while(start<end)
-       {
-           int sum=nums[start]+nums[end];
-           if(sum==target)
-           {
-               vector<int> res;
-               res.push_back(value);
-               res.push_back(nums[start]);
-               res.push_back(nums[end]);
-               answer.push_back(res);
-               while(start<end&&nums[start]==nums[start+1])
-                {
-                    start++;
-                }
-                start++;
-                while(start<end&&nums[end]==nums[end-1])
-                {
-                    end--;
-                }
-                end--;
-           }
-           else if(sum>target)
-           {
-               end--;
-           }
-           else
-           {
-               start++;
-           }
-       }
-       return answer;
-   }
-    vector<vector<int>> threeSum(vector<int>& nums) {
-            vector<vector<int> > vec;
-            sort(nums.begin(),nums.end());
-            int size=nums.size();
-            if(size<3)
-                return vec;
-            for(int i=0;i<size;i++)
+    vector<vector<int> > twosum(vector<int>& nums,int start,int target)
+    {
+        int low=start;
+        int high=nums.size()-1;
+        vector<vector<int> > res;
+        while(low<high)
+        {
+            int sum=nums[low]+nums[high];
+            int left=nums[low];
+            int right=nums[high];
+            if(sum<target)
             {
-            if(i>0&&nums[i]==nums[i-1])
+                while(low<high&&nums[low]==left)//重复
+                low++;
+            }
+            else if(sum>target)
             {
-                continue;
+                while(low<high&&nums[high]==right)//重复
+                high--;
             }
-            auto row=twosum(nums,i+1,size-1,-nums[i],nums[i]);
-            vec.insert(vec.end(),row.begin(),row.end());
+            else
+            {
+                vector<int> temp;
+                temp.push_back(nums[low]);
+                temp.push_back(nums[high]);
+                res.push_back(temp);
+                while(low<high&&nums[low]==left)//重复
+                    low++;
+                while(low<high&&nums[high]==right)//重复
+                    high--;
             }
-            return vec;
+        }
+        return res;
+    }
+    vector<vector<int> > threeSum(vector<int>& nums) {
+        vector<vector<int> > res;
+        int n=nums.size();
+        if(n==0)
+            return res;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++)
+        {
+            vector<vector<int> > tuple=twosum(nums,i+1,0-nums[i]);
+            for(int j=0;j<tuple.size();j++)
+            {
+                tuple[j].push_back(nums[i]);//注意ij
+                res.push_back(tuple[j]);
+            }
+            while(i<n-1&&nums[i]==nums[i+1])//防止nums[i]重复
+                i++;
+        }
+        return res;
     }
 };

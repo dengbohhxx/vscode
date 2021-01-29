c08121174d8a420bf722cc0f0012ae2d6028b57d
dengbohhxx <dengbohhxx@sina.com>
2021-01-29 20:54:03 +0800

    反转链表

---
+++ leetcode/反转链表.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL)
+            return NULL;
+        ListNode * p=head->next;
+        ListNode * newhead=head;
+        while(p!=NULL)
+        {
+            head->next=p->next;
+            p->next=newhead;
+            newhead=p;
+            p=head->next;
+        }
+        return  newhead;
+    }
+};
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL)
+            return NULL;
+        ListNode * pre=NULL;
+        ListNode * p=NULL;
+        while(head!=NULL)
+        {
+            p=head->next;
+            head->next=pre;
+            pre=head;
+            head=p;
+        }
+        return pre;
+    }
+};
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL)
+            return NULL;
+        ListNode * pre=NULL;
+        ListNode * p=NULL;
+        while(head!=NULL)
+        {
+            p=head->next;
+            head->next=pre;
+            pre=head;
+            head=p;
+        }
+        return pre;
+    }
+};

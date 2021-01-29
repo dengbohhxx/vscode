4e4f124d93e97197a6902ce6747f10c9ec502f8b
dengbohhxx <dengbohhxx@sina.com>
2021-01-29 20:51:10 +0800

    计数质数

---
+++ leetcode/计数质数.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isprimes(int n)
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int countPrimes(int n) {
+        int count=0;
+        for(int i=2;i<n;i++)
+        {
+            if(isprimes(i))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};

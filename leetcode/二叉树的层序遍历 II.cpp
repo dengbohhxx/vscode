230f4d0be30712a58e76d895e79cc1852bf1d791
dengbohhxx <dengbohhxx@sina.com>
2021-02-04 09:50:04 +0800

    二叉树的层序遍历 II

---
+++ leetcode/二叉树的层序遍历 II.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<int> row;
+        vector<vector<int> > vec;
+        if(root==NULL)
+            return vec;
+        TreeNode * current=NULL;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty())
+        {
+            int size=que.size();
+            for(int i=0;i<size;i++)
+            {
+                current=que.front();
+                que.pop();
+                row.push_back(current->val);
+                if(current->left!=NULL)
+                {
+                    que.push(current->left);
+                }
+                if(current->right!=NULL)
+                {
+                    que.push(current->right);
+                }
+            }
+            vec.push_back(row);
+            row.clear();
+        }
+        reverse(vec.begin(),vec.end());
+        return vec;
+    }
+};

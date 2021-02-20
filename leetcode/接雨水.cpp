d89808b6b7a33dad06a5847ab22af2863f912c09
dengbohhxx <dengbohhxx@sina.com>
2021-02-20 16:48:18 +0800

    接雨水

--- leetcode/接雨水.cpp
+++ leetcode/接雨水.cpp
@@ -46,6 +46,33 @@ public:
         return ans;
     }
 };
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        if(n==0)
+            return 0;
+        int ans=0;
+        int current=0;
+        stack<int> stk;
+        while(current<n)
+        {
+            while(!stk.empty()&&height[current]>height[stk.top()])
+            {
+                int top=stk.top();
+                stk.pop();
+                if(stk.empty())
+                    break;
+                int distance=current-stk.top()-1;
+                int bounded_height=min(height[current],height[stk.top()])-height[top];
+                ans+=distance*bounded_height;
+            }
+            stk.push(current);
+            current++;
+        }
+        return ans;     
+    }
+};
 */
 class Solution {
 public:
@@ -53,5 +80,26 @@ public:
         int n=height.size();
         if(n==0)
             return 0;
-        
+        int left=0;
+        int right=n-1;
+        int lmax=height[0];
+        int rmax=height[n-1];
+        int ans=0;
+        while(left<=right)
+        {
+            lmax=max(lmax,height[left]);
+            rmax=max(rmax,height[right]);
+            if(lmax<rmax)
+            {
+                ans+=lmax-height[left];
+                left++;
+            }
+            else
+            {
+                ans+=rmax-height[right];
+                right--;
+            }
+        }
+        return ans;
+    }
 };

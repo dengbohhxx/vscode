514cf29c038e19d83d6822ca71c44d48cfbd5019
dengbohhxx <dengbohhxx@sina.com>
2021-03-05 17:48:25 +0800

    杨辉三角 II

---
+++ leetcode/杨辉三角 II.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int> > dp(rowIndex+1,vector<int>(rowIndex+1,0));
+        for(int i=0;i<=rowIndex;i++)
+        {
+            dp[i][0]=1;
+            dp[i][i]=1;
+        }
+        for(int i=1;i<=rowIndex;i++)
+        {
+            for(int j=1;j<i;j++)
+            {
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+            }
+        }
+        vector<int> vec;
+        for(int i=0;i<=rowIndex;i++)
+        {
+            vec.push_back(dp[rowIndex][i]);
+        }
+        return vec;
+    }
+};

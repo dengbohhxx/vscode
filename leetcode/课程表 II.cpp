2d98c5e70c1de3530821f97e44b8d7b0f94cdebd
dengbohhxx <dengbohhxx@sina.com>
2021-03-29 19:32:24 +0800

    课程表 II

---
+++ leetcode/课程表 II.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void dfs(int u)
+    {
+        visited[u]=1;
+        for(int i=0;i<edges[u].size();i++)
+        {
+            if(visited[edges[u][i]]==0)
+            {
+                dfs(edges[u][i]);
+                if(!valid)
+                {
+                    return;
+                }
+            }
+            else if(visited[edges[u][i]]==1)
+                {
+                    valid=false;
+                    return;
+                }
+        }
+        visited[u]=2;
+        result.push_back(u);
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        edges.resize(numCourses);
+        visited.resize(numCourses);
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            edges[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        for(int i=0;i<numCourses&&valid;i++)
+        {
+            if(!visited[i])
+            {
+                dfs(i);
+            }
+        }
+        if(!valid)
+            return {};
+        reverse(result.begin(),result.end());
+        return result;
+    }
+private:
+    vector<vector<int> > edges;
+    vector<int> visited;
+    vector<int> result;
+    bool valid=true;
+};

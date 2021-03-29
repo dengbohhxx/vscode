38c91e521ca433c6834359f6b14dcc9401791423
dengbohhxx <dengbohhxx@sina.com>
2021-03-29 20:43:03 +0800

    猜数字大小

---
+++ leetcode/猜数字大小.cpp
@@ -0,0 +1,33 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l=1;
+        int r=n;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(guess(mid)==0)
+            {
+                return mid;
+            }
+            else if(guess(mid)>0)
+            {
+                l=mid+1;
+            }
+            else if(guess(mid)<0)
+            {
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+};

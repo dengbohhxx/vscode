98f5df5a97705868f8337485e4048841df69f05c
dengbohhxx <dengbohhxx@sina.com>
2021-04-03 11:54:00 +0800

    交错字符串

---
+++ leetcode/交错字符串.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int m=s1.size();
+        int n=s2.size();
+        int t=s3.size();
+        if(m+n!=t)
+            return false;
+        vector<vector<bool> > dp(m+1,vector<bool>(n+1,false));
+        dp[0][0]=true;
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                int p=i+j-1;
+                if(i>0)
+                        dp[i][j] = dp[i][j]|(dp[i-1][j]&&(s1[i-1]==s3[p]));
+                if(j>0)
+                        dp[i][j] = dp[i][j]|(dp[i][j-1]&&(s2[j-1]==s3[p]));
+            }
+        }
+        return dp[m][n];
+    }
+};
